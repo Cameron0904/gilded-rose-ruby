c3a1b210673862c1bd3a90ae53218d9f227d1337
Cameron Cox <cameronacox0904@gmail.com>
2024-02-27 17:08:23 -0600

    Updated brie_tick method in order to return back to green on test cases

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -13,6 +13,10 @@ class GildedRose
     @quality -= 1 if @days_remaining <= 0
   end
   def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0 && @quality < 50
   end
   def tick
     case name
