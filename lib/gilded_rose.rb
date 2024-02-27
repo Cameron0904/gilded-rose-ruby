7f37c86495d171591512a51e0a5a3e68ed4c9913
Cameron Cox <cameronacox0904@gmail.com>
2024-02-27 17:57:06 -0600

    Added Normal Class and normal tick method was modified with class behavior

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,25 +1,42 @@
 class GildedRose
   attr_reader :name, :days_remaining, :quality
-
+    
   def initialize(name:, days_remaining:, quality:)
     @name = name
     @days_remaining = days_remaining
     @quality = quality
   end
+  
   def normal_tick
-    @days_remaining -= 1
-    return if @quality == 0
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+    @item = Normal.new(quality,days_remaining)
+    item.tick
   end
+  
+  
+  class Normal
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality,days_remaining)
+      @quality, @days_remaining = quality,days_remaining
+    end
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
+  end
+
   def brie_tick
     @days_remaining -= 1
     return if @quality >= 50
     @quality += 1
     @quality += 1 if @days_remaining <= 0 && @quality < 50
   end
+
   def sulfuras_tick
   end
+  
   def backstage_tick
     @days_remaining -= 1
     return if @quality >= 50
