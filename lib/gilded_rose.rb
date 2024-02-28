c699497b1dbec8172ab1d0a7d2b3b9116fe2adf7
Cameron Cox <cameronacox0904@gmail.com>
2024-02-27 18:08:02 -0600

    Added Backstage Class and modified backstage_tick

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -19,6 +19,10 @@ class GildedRose
     @item = Sulfuras.new(quality,days_remaining)
     item.tick
   end
+  def backstage_tick
+    @item = Backstage.new(quality,days_remaining)
+    item.tick
+  end
   
   
   class Normal
@@ -56,16 +60,24 @@ class GildedRose
     def tick
     end
   end
+  class Backstage
+    attr_reader :quality, :days_remaining
+    
+    def initialize(quality,days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
   
-  def backstage_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
+    end
   end
+
 
   def tick
     case name
